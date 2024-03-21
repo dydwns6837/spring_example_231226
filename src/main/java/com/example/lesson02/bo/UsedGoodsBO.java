b298d689f61fc3f70611b9748010d8b42608e1d2
test <test@test.com>
2024-03-21 21:42:51 +0900

    240321 내용추가

---
+++ src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
@@ -0,0 +1,10 @@
+package com.example.lesson02.bo;
+
+import org.springframework.stereotype.Service;
+
+@Service // Spring bean
+public class UsedGoodsBO {
+	
+	// input: (Controller로 부터 받아온다.) x
+	// output: (Controller한테 준다.) List<UsedGoods>
+}
