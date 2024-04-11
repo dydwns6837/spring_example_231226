d7d326dff8a60d0542ff2860e7413440b9355ee7
test <test@test.com>
2024-04-11 21:40:51 +0900

    240411 내용추가

--- src/main/java/com/example/lesson04/bo/StudentBO.java
+++ src/main/java/com/example/lesson04/bo/StudentBO.java
@@ -1,10 +1,15 @@
 package com.example.lesson04.bo;
 
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.lesson04.domain.Student;
 import com.example.lesson04.mapper.StudentMapper;
+import com.example.lesson07.entity.StudentEntity;
+import com.example.lesson07.repository.StudentRepository;
 
 @Service
 public class StudentBO {
@@ -19,6 +24,15 @@ public class StudentBO {
 	public StudentEntity addStudent(
 			String name, String phoneNumber, String email, String dreamJob) {
 		
+		StudentEntity student = StudentEntity.builder()
+				.name(name)
+				.phoneNumber(phoneNumber)
+				.email(email)
+				.dreamJob(dreamJob)
+				.createdAt(ZonedDateTime.now())  // @UpdateTimestamp 있으면 생략 가능
+				.build();
+		
+		return studentRepository.save(student);
 	}
 	
 	
@@ -27,8 +41,39 @@ public class StudentBO {
 		studentMapper.insertStduent(student);
 	}
 	
+	// update
+	public StudentEntity updateStudentDreamJobById(int id, String dreamJob) {
+		// 기존 데이터를 조회해온다.								비어있을때 null 이 나올것이다.
+		StudentEntity student = studentRepository.findById(id).orElse(null);
+		
+		// 데이터의 값을 변경한다 => 엔티티에
+		if (student != null) {
+			student = student.toBuilder() // toBuilder는 기존 필드값은 유지하고 일부만 변경
+				.dreamJob(dreamJob)
+				.build(); // 꼭 다시 저장
+			
+			student = studentRepository.save(student);
+		}
+		
+		// save(엔티티객체) => id가 채워져있기때문에 update 가 알아서 됨.
+		return student; // null or StudentEntity
+	}
+	
+	
 	// input: id , output:student or null
 	public Student getStudentById(int id) {
 		return studentMapper.selectStudentById(id);
+	}
+	
+	public void deleteStudentById(int id) {
+		// 1번째 방법
+//		StudentEntity student = studentRepository.findById(id).orElse(null);
+//		if (student != null) {
+//			studentRepository.delete(student);
+//		}
+		
+		// 2번째 방법
+		Optional<StudentEntity> studentOptiondal = studentRepository.findById(id);
+		studentOptiondal.ifPresent(s -> studentRepository.delete(s));
 	}
 }
--- src/main/java/com/example/lesson07/Lesson07Ex01RestController.java
+++ src/main/java/com/example/lesson07/Lesson07Ex01RestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.lesson04.bo.StudentBO;
+import com.example.lesson07.entity.StudentEntity;
 
 @RequestMapping("/lesson07")
 @RestController
@@ -22,6 +23,22 @@ public class Lesson07Ex01RestController {
 		String email = "dydwns6837@naver.com";
 		String dreamJob = "개발자";
 		
-		studentBO.
+		// save 된 객체를 리턴하므로 id값이 채워져 있다.
+		return studentBO.addStudent(name, phoneNumber, email, dreamJob);
+	}
+	
+	// U: Update
+	@GetMapping("/ex01/update")
+	public StudentEntity update() {
+		// id가 5번인 dreamJob 변경
+		return studentBO.updateStudentDreamJobById(5, "운동선수");
+	}
+	
+	// D: Delete
+	@GetMapping("/ex01/delete")
+	public String delete() {
+		// id:4 삭제
+		studentBO.deleteStudentById(4);
+		return "삭제 성공";
 	}
 }
